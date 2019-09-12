20135d74858026809fd7d8a9b41c518fe90ad254
Fernando de Paula <di.paulaf@hotmail.com>
2019-09-12 19:12:14 -0300

    app lista tarefas concluido

--- Udemy/Android Nativo/ListadeTarefas/app/src/main/java/br/com/udemy/listadetarefas/model/Tarefa.java
+++ Udemy/Android Nativo/ListadeTarefas/app/src/main/java/br/com/udemy/listadetarefas/model/Tarefa.java
@@ -4,16 +4,16 @@ import java.io.Serializable;
 
 //ja que será enviado via intent, deve ser serializavel
 public class Tarefa implements Serializable {
-    private long id;
+    private Long id;
     private String nomeTarefa;
 
     public Tarefa() {}
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
