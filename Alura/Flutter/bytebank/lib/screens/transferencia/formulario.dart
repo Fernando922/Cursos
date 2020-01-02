5471114f93ea19505d7265d0e27ccdcb47c2e7d0
Fernando de Paula <di.paulaf@hotmail.com>
2020-01-02 15:00:05 -0300

    app tarefas sem excluir e editar

--- Alura/Flutter/bytebank/lib/screens/transferencia/formulario.dart
+++ Alura/Flutter/bytebank/lib/screens/transferencia/formulario.dart
@@ -25,30 +25,31 @@ class FormularioTransferenciaState extends State<FormularioTransferencia> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(_tituloAppBar),
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: _rotuloCampoNumeroConta,
+              dica: _dicaCampoNumeroConta,
+            ),
+            Editor(
+              controlador: _controladorCampoValor,
+              rotulo: _rotuloCampoValor,
+              dica: _dicaCampoValor,
+              icone: Icons.monetization_on,
+            ),
+            RaisedButton(
+              child: Text(_textoBotaoConfirmar),
+              onPressed: () => _criaTransferencia(context),
+            )
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Editor(
-                controlador: _controladorCampoNumeroConta,
-                rotulo: _rotuloCampoNumeroConta,
-                dica: _dicaCampoNumeroConta,
-              ),
-              Editor(
-                controlador: _controladorCampoValor,
-                rotulo: _rotuloCampoValor,
-                dica: _dicaCampoValor,
-                icone: Icons.monetization_on,
-              ),
-              RaisedButton(
-                child: Text(_textoBotaoConfirmar),
-                onPressed: () => _criaTransferencia(context),
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 
   void _criaTransferencia(BuildContext context) {
