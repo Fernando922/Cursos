2897277e6c1f0e946d86428ce961bc31854f2116
Fernando de Paula <di.paulaf@hotmail.com>
2020-01-16 13:17:09 -0300

    1.13 video front

--- Rocketseat/oministack-rocketseat-devMap/web/src/App.js
+++ Rocketseat/oministack-rocketseat-devMap/web/src/App.js
@@ -1,31 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./global.css";
 import "./app.css";
 import "./Sidebar.css";
+import "./Main.css";
+
 
 function App() {
+
+  const [coords, setCoords] = useState({})
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 3000
+      }
+    )
+  },[])
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastar</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input name="github_username" id="github_username" required />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">latitude</label>
               <input name="latitude" id="latitude" required />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">longitude</label>
               <input name="longitude" id="longitude" required />
             </div>
@@ -34,7 +53,80 @@ function App() {
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/35746707?s=460&v=4"
+                alt="Josiel"
+              />
+              <div className="user-info">
+                <strong>Josiel Alves</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Back end</p>
+            <a href="https://google.com">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/35746707?s=460&v=4"
+                alt="Josiel"
+              />
+              <div className="user-info">
+                <strong>Josiel Alves</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Back end</p>
+            <a href="https://google.com">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/35746707?s=460&v=4"
+                alt="Josiel"
+              />
+              <div className="user-info">
+                <strong>Josiel Alves</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Back end</p>
+            <a href="https://google.com">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/35746707?s=460&v=4"
+                alt="Josiel"
+              />
+              <div className="user-info">
+                <strong>Josiel Alves</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Back end</p>
+            <a href="https://google.com">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/35746707?s=460&v=4"
+                alt="Josiel"
+              />
+              <div className="user-info">
+                <strong>Josiel Alves</strong>
+                <span>ReactJs, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Back end</p>
+            <a href="https://google.com">Acessar perfil no github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
